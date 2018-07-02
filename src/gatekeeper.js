ab5d09dd5e1d6bafc5c73e04e3fc0d9cf29081a3
Trey Miller <Trey_M@heitkamp.corp>
2018-07-02 11:00:21 -0500

    Added Some Documentation

--- src/gatekeeper.js
+++ src/gatekeeper.js
@@ -6,21 +6,25 @@ class GateKeeper {
     }
 
     Require(requiredParameters, optionalParameters) {
+
         return function(req, res, next){
 
-            let missing = [];
+            let missingRequired = [];
             
+            // Check if request contains required parameters
             requiredParameters.forEach(p => {
-                if(!req.body[p]) {missing.push(p);}
+                if(!req.body[p]) {missingRequired.push(p);}
             });
 
-            if(missing.length > 0 ){
-                res.status(400).send('Bad Request. \n Missing the following parameters: \n' + missing);
+            // If missing any required parameters, stop the request
+            if(missingRequired.length > 0 ){
+                res.status(400).send('Bad Request. <br /> Missing the following parameters: \n' + missingRequired);
             }else{
                 next();
             }
             
         }
+
     }
 
 }
